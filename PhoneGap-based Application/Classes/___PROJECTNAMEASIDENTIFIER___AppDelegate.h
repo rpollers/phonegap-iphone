067f6b64dfa6a7dcfaa320628ec067d914018995
Jesse <jesse.macfadyen@nitobi.com>
2011-03-07 14:20:27 -0800

    added comment explaining things

--- PhoneGap-based Application/Classes/___PROJECTNAMEASIDENTIFIER___AppDelegate.h
+++ PhoneGap-based Application/Classes/___PROJECTNAMEASIDENTIFIER___AppDelegate.h
@@ -16,6 +16,9 @@
 
 // invoke string is passed to your app on launch, this is only valid if you 
 // edit ___PROJECTNAME___.plist to add a protocol
+// a simple tutorial can be found here : 
+// http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
+
 @property (copy)  NSString* invokeString;
 
 @end
